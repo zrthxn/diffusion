1675e7d76fd6f2c7baa5b62571dbf0a66b446e84
Alisamar Husain <alisamar.husain@gtmhub.com>
2023-01-13 20:33:45 +0530

    Fix nograd

--- data/__init__.py
+++ data/__init__.py
@@ -13,34 +13,34 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx) -> Tensor:
         return self.images[idx]
 
-    @no_grad()
     @staticmethod
     def plot(images: list, res = 4, denorm = True, save = None):
-        ncols = min(len(images), 8)
-        nrows = len(images) // ncols
-        if ncols * nrows < len(images):
-            nrows += 1
+        with no_grad():
+            ncols = min(len(images), 8)
+            nrows = len(images) // ncols
+            if ncols * nrows < len(images):
+                nrows += 1
 
-        fig, ax = plt.subplots(nrows, ncols, 
-            figsize=(res * ncols, res * nrows),
-            sharey=True,
-            sharex=True)
+            fig, ax = plt.subplots(nrows, ncols, 
+                figsize=(res * ncols, res * nrows),
+                sharey=True,
+                sharex=True)
 
-        fig.set_dpi(240)
-        axes: List[plt.Axes] = ax.flatten()
-        for img, ax in zip(images, axes):
-            img = img.detach().permute(1, 2, 0)
-            if denorm:
-                img = (img + 1.) / 2.
-                img = clamp(img, 0.0, 1.0)
-            ax.imshow(img)
-            ax.set_axis_off()
-        
-        fig.tight_layout(pad=2.0)
-        if save is None:
-            plt.show()
-        else:
-            plt.savefig(save)
+            fig.set_dpi(240)
+            axes: List[plt.Axes] = ax.flatten()
+            for img, ax in zip(images, axes):
+                img = img.detach().permute(1, 2, 0)
+                if denorm:
+                    img = (img + 1.) / 2.
+                    img = clamp(img, 0.0, 1.0)
+                ax.imshow(img)
+                ax.set_axis_off()
+            
+            fig.tight_layout(pad=2.0)
+            if save is None:
+                plt.show()
+            else:
+                plt.savefig(save)
 
     def head(self):
         self.plot(self.images[:5])
